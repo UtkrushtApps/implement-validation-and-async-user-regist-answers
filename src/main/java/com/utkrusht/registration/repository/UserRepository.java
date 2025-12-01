6f7b7304fd596b5b7e3e79f81bec4cb87273ce64
Naman Muley <naman.g.muley@gmail.com>
2025-12-01 21:09:05 +0530

    Add src/main/java/com/utkrusht/registration/repository/UserRepository.java

---
+++ src/main/java/com/utkrusht/registration/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.utkrusht.registration.repository;
+
+import com.utkrusht.registration.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
