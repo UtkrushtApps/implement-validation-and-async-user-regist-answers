155afc579852d67a23be06f5619582dbb7ba1a0c
Naman Muley <naman.g.muley@gmail.com>
2025-12-01 21:09:32 +0530

    Add src/main/java/com/utkrusht/registration/RegistrationApplication.java

---
+++ src/main/java/com/utkrusht/registration/RegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.utkrusht.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class RegistrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationApplication.class, args);
+    }
+}
