b7ad275a7c56baa3e631c936ab60cc443974504d
Naman Muley <naman.g.muley@gmail.com>
2025-12-01 21:09:15 +0530

    Add src/main/java/com/utkrusht/registration/service/DuplicateEmailException.java

---
+++ src/main/java/com/utkrusht/registration/service/DuplicateEmailException.java
@@ -0,0 +1,7 @@
+package com.utkrusht.registration.service;
+
+public class DuplicateEmailException extends RuntimeException {
+    public DuplicateEmailException(String message) {
+        super(message);
+    }
+}
